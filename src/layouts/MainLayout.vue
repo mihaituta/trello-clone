a734c7e207f8641ac2794e6381b7980775e813ac
Mihai Tuță <tutamihai@gmail.com>
2021-10-12 18:15:38 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hhh lpR fFf">
+
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar square>
+            <img src="../assets/icon.png" alt="Trello logo">
+          </q-avatar>
+          Trello
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer mini v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
